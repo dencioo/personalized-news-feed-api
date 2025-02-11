ac59ada450d38dee1179c54c77a2e470e72e3cbd
Dean <paldeanharley@gmail.com>
2025-02-11 23:07:23 +0800

    Creating views, defining the urls

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,11 @@
 # Contains views that define the app's behavior
 from django.shortcuts import render
+from rest_framework import generics
+from .models import BlogPost
+from .serializers import BlogPostSerializer
 
 # Create your views here.
+
+class BlogPostListCreate(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
