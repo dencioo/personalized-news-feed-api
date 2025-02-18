0cfb63a79a21b8561630febe5ce0876af035c1aa
dencioo <paldeanharley@gmail.com>
2025-02-18 15:49:03 +0800

    Added a few models

--- feed/models.py
+++ feed/models.py
@@ -1,5 +1,6 @@
 # Defines the database models for the App
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class BlogPost(models.Model):
@@ -9,3 +10,15 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Article(models.Model):
+    # Extend the users model to store the preferences
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+    preferred_categories = models.ManyToManyField('Category')
+    preferred_sources = models.JSONField(default=list)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
