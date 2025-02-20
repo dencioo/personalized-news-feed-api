b659b196926388b1b6332bd2629c8b908f63c4c8
dencioo <paldeanharley@gmail.com>
2025-02-20 17:48:26 +0800

    Added comments for better understanding

--- feed/tests/test_models.py
+++ feed/tests/test_models.py
@@ -2,12 +2,13 @@ import pytest
 from feed.models import Article, Category
 from django.contrib.auth.models import User
 
-@pytest.mark.django_db
-def test_create_article():
-    user = User.objects.create(username="testuser")
-    category = Category.objects.create(name="Technology")
-    article = Article.objects.create(user=user)
-    article.preferred_categories.add(category)
 
-    assert article.user.username == "testuser"
-    assert category in article.preferred_categories.all()
+@pytest.mark.django_db # this allows the test to interact with database
+def test_create_article():  # prefixing with 'test_' and runs the function with 'test_'; to check if an "Article" can created, linked to a "User" and assigned categories
+    user = User.objects.create(username="testuser") # to insert a new user to the database, but no password/email; this steps ensures we have valid user before creating article
+    category = Category.objects.create(name="Technology") # to insert Category since it belong to multiple categories
+    article = Article.objects.create(user=user) # creates a new article in the database, and this article belongs to the user we just created (testuser)
+    article.preferred_categories.add(category) # Add() method is used because preferred_categories is a ManytoManyField
+
+    assert article.user.username == "testuser" # check if it matches "testuser"
+    assert category in article.preferred_categories.all() # returns a list of all categories; checks if the "Technology" is in that list
