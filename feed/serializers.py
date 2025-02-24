0854579c57332fe7c1e6588649a1c001a3485006
dencioo <paldeanharley@gmail.com>
2025-02-24 23:21:38 +0800

    Added Category and UserProfile serializers

--- feed/serializers.py
+++ feed/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
-from .models import BlogPost
+from .models import BlogPost, UserProfile, Category
 
 class BlogPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
         fields = ["id", "title", "content", "published_date"]
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    preffered_categories = CategorySerializer(many=True, readonly=True)
+
+    class Meta:
+        model = UserProfile
+        fields = ['id', 'user', 'preferred_categories', 'preferred_sources']
