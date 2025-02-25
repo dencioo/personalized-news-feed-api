a6302ef4c3ded5a955b376bcdde34fceaa9e8088
dencioo <paldeanharley@gmail.com>
2025-02-25 22:27:50 +0800

    Created views for my models in views.py

--- feed/views.py
+++ feed/views.py
@@ -1,11 +1,19 @@
 # Contains views that define the app's behavior
 from django.shortcuts import render
-from rest_framework import generics
-from .models import BlogPost
-from .serializers import BlogPostSerializer
+from rest_framework import viewsets
+from .models import BlogPost, UserProfile, Category
+from .serializers import BlogPostSerializer, UserProfileSerializer, CategorySerializer
 
 # Create your views here.
 
-class BlogPostListCreate(generics.ListCreateAPIView):
+class BlogPostListCreate(viewsets.ModelViewSet):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer 
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
