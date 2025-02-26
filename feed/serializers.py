f87fb59f74908e14d93736608bbfac9f0953edfd
dencioo <paldeanharley@gmail.com>
2025-02-26 18:03:37 +0800

    Updated my serializers.py to include serializer for user registration

--- feed/serializers.py
+++ feed/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import BlogPost, UserProfile, Category
 
 class BlogPostSerializer(serializers.ModelSerializer):
@@ -17,3 +18,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ['id', 'user', 'preferred_categories', 'preferred_sources']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'email']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
