905bdc36a62367f4d1f4c7e026d2064b0799d125
dencioo <paldeanharley@gmail.com>
2025-03-16 22:12:59 +0800

    fix: allow updating preferred categories in UserProfileSerializer

--- feed/models.py
+++ feed/models.py
@@ -2,7 +2,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class UserProfile(models.Model): # models.Model is the base class for all models in Django
     # Extend the users model to store the preferences
     user = models.OneToOneField(User, on_delete=models.CASCADE) 
--- feed/serializers.py
+++ feed/serializers.py
@@ -12,10 +12,20 @@ class UserProfileSerializer(serializers.ModelSerializer):   # Serialize for the 
     # This insert the info about each news category in the user's prefrence
     preferred_categories = CategorySerializer(many=True, read_only=True) 
 
+    preferred_categories_ids = serializers.PrimaryKeyRelatedField(
+        queryset = Category.objects.all(), # Queryset to get all the categories
+        many = True, # Allow for multiple categories
+        write_only = True # Field is not included in the serialized data
+    )
     class Meta:
         model = UserProfile
         fields = ['id', 'user', 'preferred_categories', 'preferred_sources'] # Specified fields to be included in Serialized data
 
+    def update(self, instance, validated_data):
+        preferred_categories = validated_data.pop('preferred_categories_ids', None)
+        instance.preferred_categories.set(preferred_categories) # preferred_categories is a many-to-many field
+        return super().update(instance, validated_data) # Call the parent class update method
+        
 class UserSerializer(serializers.ModelSerializer): # Serialize for the User model
     class Meta:
         model = User # Link the serializer to Django's buit-in User model
